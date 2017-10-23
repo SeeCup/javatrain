4ecf7a41a9baa223f3cc4409f57bbffa258400a2
unknown <1239252862@qq.com>
2017-10-23 19:30:14 +0800

    初次提交

---
+++ Luncene/Lucene01/src/main/java/win/bigdream/lucene/Indexer.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package win.bigdream.lucene;
+
+import org.apache.lucene.index.IndexWriter;
+
+/**
+ * @author hh
+ *
+ */
+public class Indexer {
+	
+	private IndexWriter write;
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
